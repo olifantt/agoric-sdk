349e7119272292db308c432c7172958d682c5f45
Kate Sills <kate@agoric.com>
2021-03-05 11:06:35 -0800

    chore: add tests of basic functionality in Swingset vat env to ERTP

---
+++ packages/ERTP/test/swingsetTests/basicFunctionality/test-basicFunctionality.js
@@ -0,0 +1,26 @@
+import test from 'ava';
+import { loadBasedir, buildVatController } from '@agoric/swingset-vat';
+import path from 'path';
+
+async function main(basedir, argv) {
+  const dir = path.resolve(`${__dirname}/..`, basedir);
+  const config = await loadBasedir(dir);
+  const controller = await buildVatController(config, argv);
+  await controller.run();
+  return controller.dump();
+}
+
+const expected = [
+  'start test basic functionality',
+  'isLive: true',
+  'getAmountOf: {"brand":{},"value":1000}',
+  'newPayment amount: {"brand":{},"value":1000}',
+  'burned amount: {"brand":{},"value":200}',
+  'claimedPayment amount: {"brand":{},"value":200}',
+  'combinedPayment amount: {"brand":{},"value":600}',
+];
+
+test('test splitPayments', async t => {
+  const dump = await main('basicFunctionality', ['basicFunctionality']);
+  t.deepEqual(dump.log, expected);
+});
